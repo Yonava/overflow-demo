64004462204912d8fb51526620700bae571e26d6
yonava <yonavossandreae@gmail.com>
2025-11-29 21:54:51 -0800

    glow up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const doneThinking = () => {
+    setValue('This is my answer')
+  }
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
+      <button onClick={doneThinking}>Done</button>
       <div className="ufo-container">
+        {!value && <div className="ufo-matte glow"></div>}
+        <input value={value} type="text" placeholder='Generating...' className={[value ? '' : 'thinking', 'ufo-text'].join(' ')} />
+      </div>
+
+      {/* <div className="ufo-container">
         <div className="ufo-matte glow"></div>
         <div className="ufo-glow"></div>
-        <input type="text" placeholder="UFO" className='ufo-text' />
-      </div>
+        <input value={value} type="text" className='ufo-text' />
+      </div> */}
     </div>
   )
 }
